d3e06f80ad9c000af3d272192a72fa2858432b31
Liliia Matviichuk <liliia.matviichuk@eleks.com>
2019-12-23 13:40:17 +0200

    Add testNG.xml

---
+++ src/test/java/StudioAPITests.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import static org.testng.Assert.*;
+
+public class StudioAPITests {
+
+    private String baseURL = "https://studio-cg.qa.xyleme.com/editor";
+
+}
